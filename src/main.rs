34eafac163702a693e8150bcb4978f2e9e7f823e
Grigorii Efimov <Grigorii_Efimov@epam.com>
2020-05-02 18:42:44 +0300

    Add protocol

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::thread;
 use std::time::Duration;
 extern crate chrono;
 use chrono::Utc;
+use std::fs;
 // use std::io;
 
 const DELAY: u64 = 0;
@@ -22,10 +23,17 @@ struct Result2 {
 
 fn main() {
     let (tx, rx) = mpsc::channel::<i32>();
+    let protocol = Arc::new(Mutex::new(Vec::<String>::new()));
 
-    thread::spawn(|| a_task(tx));
+    let pa = Arc::clone(&protocol);
+    thread::spawn(|| a_task(tx, pa));
 
     println!("Result: {}", rx.iter().next().unwrap());
+
+    const RESULT_PATH: &'static str = "protocol.txt";
+    let data = protocol.lock().unwrap().join("\n");
+    fs::write(RESULT_PATH, data).unwrap();
+    println!("Completed! The protocol is in the {}", RESULT_PATH);
 
     // let mut option = String::new();
     // match io::stdin().read_line(&mut option) {
@@ -34,14 +42,27 @@ fn main() {
     // }
 }
 
-fn a_task(tx: mpsc::Sender<i32>) {
-    println!("A started at {}", Utc::now().time());
-
-    thread::sleep(Duration::from_millis(DELAY));
+fn a_task(tx: mpsc::Sender<i32>, p: Arc<Mutex<Vec<String>>>) {
+    let mut protocol = p.lock().unwrap();
+    let start = format!("A starts at {}", Utc::now().time());
+    println!("{}", start);
+    protocol.push(start);
+    drop(protocol);
 
     let m1 = [1, 2, 3];
     let m2 = [4, 5, 6];
     let m3 = [7, 8, 8];
+
+    thread::sleep(Duration::from_millis(DELAY));
+
+    let result = format!("A Result: M1: {:?}, M2: {:?}, M3: {:?}", m1, m2, m3);
+    let initiates = String::from("A initiates B, C, D");
+    let mut protocol = p.lock().unwrap();
+    println!("{}", result);
+    println!("{}", initiates);
+    protocol.push(result);
+    protocol.push(initiates);
+    drop(protocol);
 
     let result1 = Arc::new(Mutex::new(Result1 {
         f1: Option::<i32>::None,
@@ -50,29 +71,47 @@ fn a_task(tx: mpsc::Sender<i32>) {
     }));
 
     let txb = mpsc::Sender::clone(&tx);
+    let pb = Arc::clone(&p);
     let result1b = Arc::clone(&result1);
-    thread::spawn(move || b_task(txb, m1, m2, m3, result1b));
+    thread::spawn(move || b_task(txb, pb, m1, m2, m3, result1b));
     let txc = mpsc::Sender::clone(&tx);
+    let pc = Arc::clone(&p);
     let result1c = Arc::clone(&result1);
-    thread::spawn(move || c_task(txc, m1, m2, m3, result1c));
-    thread::spawn(move || d_task(tx, m1, m2, m3, result1));
+    thread::spawn(move || c_task(txc, pc, m1, m2, m3, result1c));
+    let pd = Arc::clone(&p);
+    thread::spawn(move || d_task(tx, pd, m1, m2, m3, result1));
 
-    println!("A ends at {}", Utc::now().time());
+    let mut protocol = p.lock().unwrap();
+    let end = format!("A ends at {}", Utc::now().time());
+    println!("{}", end);
+    protocol.push(end);
 }
 
 fn b_task(
     tx: mpsc::Sender<i32>,
+    p: Arc<Mutex<Vec<String>>>,
     m1: [i32; 3],
     m2: [i32; 3],
     m3: [i32; 3],
     result1: Arc<Mutex<Result1>>,
 ) {
-    println!("B started at {}", Utc::now().time());
-    println!("B activated by Taks A");
+    let mut protocol = p.lock().unwrap();
+    let start = format!("B starts at {}", Utc::now().time());
+    println!("{}", start);
+    println!("B initiated by A");
+    protocol.push(start);
+    protocol.push(String::from("B initiated by A"));
+    drop(protocol);
+
+    let s = m1.iter().sum::<i32>() + m2.iter().sum::<i32>() + m3.iter().sum::<i32>();
 
     thread::sleep(Duration::from_millis(3 * DELAY));
 
-    let s = m1.iter().sum::<i32>() + m2.iter().sum::<i32>() + m3.iter().sum::<i32>();
+    let result = format!("B result: {}", s);
+    let mut protocol = p.lock().unwrap();
+    println!("{}", result);
+    protocol.push(result);
+    drop(protocol);
 
     let mut result = result1.lock().unwrap();
     result.f1 = Some(s);
@@ -80,29 +119,50 @@ fn b_task(
     match result.f2 {
         Some(b) => match result.f7 {
             Some(c) => {
-                thread::spawn(move || k_task(tx, s, b, c, "B"));
+                let mut protocol = p.lock().unwrap();
+                let initiates = String::from("B initiates K");
+                println!("{}", initiates);
+                protocol.push(initiates);
+                drop(protocol);
+                let pk = Arc::clone(&p);
+                thread::spawn(move || k_task(tx, pk, s, b, c, "B"));
             }
             None => {}
         },
         None => {}
     }
 
-    println!("B ends at {}", Utc::now().time());
+    let mut protocol = p.lock().unwrap();
+    let end = format!("B ends at {}", Utc::now().time());
+    println!("{}", end);
+    protocol.push(end);
 }
 
 fn c_task(
     tx: mpsc::Sender<i32>,
+    p: Arc<Mutex<Vec<String>>>,
     m1: [i32; 3],
     m2: [i32; 3],
     m3: [i32; 3],
     result1: Arc<Mutex<Result1>>,
 ) {
-    println!("C started at {}", Utc::now().time());
-    println!("C activated by Taks A");
+    let mut protocol = p.lock().unwrap();
+    let start = format!("C starts at {}", Utc::now().time());
+    println!("{}", start);
+    println!("C initiated by A");
+    protocol.push(start);
+    protocol.push(String::from("C initiated by A"));
+    drop(protocol);
+
+    let s = m1.iter().sum::<i32>() + m2.iter().sum::<i32>() + m3.iter().sum::<i32>();
 
     thread::sleep(Duration::from_millis(3 * DELAY));
 
-    let s = m1.iter().sum::<i32>() + m2.iter().sum::<i32>() + m3.iter().sum::<i32>();
+    let result = format!("C result: {}", s);
+    let mut protocol = p.lock().unwrap();
+    println!("{}", result);
+    protocol.push(result);
+    drop(protocol);
 
     let mut result = result1.lock().unwrap();
     result.f2 = Some(s);
@@ -110,29 +170,53 @@ fn c_task(
     match result.f1 {
         Some(a) => match result.f7 {
             Some(c) => {
-                thread::spawn(move || k_task(tx, a, s, c, "C"));
+                let mut protocol = p.lock().unwrap();
+                let initiates = String::from("C initiates K");
+                println!("{}", initiates);
+                protocol.push(initiates);
+                drop(protocol);
+                let pk = Arc::clone(&p);
+                thread::spawn(move || k_task(tx, pk, a, s, c, "C"));
             }
             None => {}
         },
         None => {}
     }
 
-    println!("C ends at {}", Utc::now().time());
+    let mut protocol = p.lock().unwrap();
+    let end = format!("C ends at {}", Utc::now().time());
+    println!("{}", end);
+    protocol.push(end);
 }
 
 fn d_task(
     tx: mpsc::Sender<i32>,
+    p: Arc<Mutex<Vec<String>>>,
     m1: [i32; 3],
     m2: [i32; 3],
     m3: [i32; 3],
     result1: Arc<Mutex<Result1>>,
 ) {
-    println!("D started at {}", Utc::now().time());
-    println!("D activated by Taks A");
+    let mut protocol = p.lock().unwrap();
+    let start = format!("D starts at {}", Utc::now().time());
+    println!("{}", start);
+    println!("D initiated by A");
+    protocol.push(start);
+    protocol.push(String::from("D initiated by A"));
+    drop(protocol);
+
+    let s = m1.iter().sum::<i32>() + m2.iter().sum::<i32>() + m3.iter().sum::<i32>();
 
     thread::sleep(Duration::from_millis(DELAY));
 
-    let s = m1.iter().sum::<i32>() + m2.iter().sum::<i32>() + m3.iter().sum::<i32>();
+    let result = format!("D result: {}", s);
+    let initiates = String::from("D initiates E, F, G");
+    let mut protocol = p.lock().unwrap();
+    println!("{}", result);
+    protocol.push(result);
+    println!("{}", initiates);
+    protocol.push(initiates);
+    drop(protocol);
 
     let result2 = Arc::new(Mutex::new(Result2 {
         f4: Option::<i32>::None,
@@ -141,140 +225,252 @@ fn d_task(
     }));
 
     let txe = mpsc::Sender::clone(&tx);
+    let pe = Arc::clone(&p);
     let result2e = Arc::clone(&result2);
     let result1e = Arc::clone(&result1);
-    thread::spawn(move || e_task(txe, s, result2e, result1e));
+    thread::spawn(move || e_task(txe, pe, s, result2e, result1e));
     let txf = mpsc::Sender::clone(&tx);
+    let pf = Arc::clone(&p);
     let result2f = Arc::clone(&result2);
     let result1f = Arc::clone(&result1);
-    thread::spawn(move || f_task(txf, s, result2f, result1f));
-    thread::spawn(move || g_task(tx, s, result2, result1));
+    thread::spawn(move || f_task(txf, pf, s, result2f, result1f));
+    let pg = Arc::clone(&p);
+    thread::spawn(move || g_task(tx, pg, s, result2, result1));
 
-    println!("D ends at {}", Utc::now().time());
+    let mut protocol = p.lock().unwrap();
+    let end = format!("D ends at {}", Utc::now().time());
+    println!("{}", end);
+    protocol.push(end);
 }
 
 fn e_task(
     tx: mpsc::Sender<i32>,
+    p: Arc<Mutex<Vec<String>>>,
     n: i32,
     result2: Arc<Mutex<Result2>>,
     result1: Arc<Mutex<Result1>>,
 ) {
-    println!("E started at {}", Utc::now().time());
-    println!("E activated by Taks D");
-
-    thread::sleep(Duration::from_millis(DELAY));
+    let mut protocol = p.lock().unwrap();
+    let start = format!("E starts at {}", Utc::now().time());
+    println!("{}", start);
+    println!("E initiated by D");
+    protocol.push(start);
+    protocol.push(String::from("E initiated by D"));
+    drop(protocol);
 
     let mut result = result2.lock().unwrap();
     let r = n * 4;
     result.f4 = Some(r);
 
+    thread::sleep(Duration::from_millis(DELAY));
+
+    let res = format!("E result: {}", r);
+    let mut protocol = p.lock().unwrap();
+    println!("{}", res);
+    protocol.push(res);
+    drop(protocol);
+
     match result.f5 {
         Some(b) => match result.f6 {
             Some(c) => {
-                thread::spawn(move || h_task(tx, r, b, c, result1, "E"));
+                let mut protocol = p.lock().unwrap();
+                let initiates = String::from("E initiates H");
+                println!("{}", initiates);
+                protocol.push(initiates);
+                drop(protocol);
+                let ph = Arc::clone(&p);
+                thread::spawn(move || h_task(tx, ph, r, b, c, result1, "E"));
             }
             None => {}
         },
         None => {}
     }
 
-    println!("E ends at {}", Utc::now().time());
+    let mut protocol = p.lock().unwrap();
+    let end = format!("E ends at {}", Utc::now().time());
+    println!("{}", end);
+    protocol.push(end);
 }
 
 fn f_task(
     tx: mpsc::Sender<i32>,
+    p: Arc<Mutex<Vec<String>>>,
     n: i32,
     result2: Arc<Mutex<Result2>>,
     result1: Arc<Mutex<Result1>>,
 ) {
-    println!("F started at {}", Utc::now().time());
-    println!("F activated by Taks D");
-
-    thread::sleep(Duration::from_millis(DELAY));
+    let mut protocol = p.lock().unwrap();
+    let start = format!("F starts at {}", Utc::now().time());
+    println!("{}", start);
+    println!("F initiated by D");
+    protocol.push(start);
+    protocol.push(String::from("F initiated by D"));
+    drop(protocol);
 
     let mut result = result2.lock().unwrap();
     let r = n * 5;
     result.f5 = Some(r);
 
+    thread::sleep(Duration::from_millis(DELAY));
+
+    let res = format!("F result: {}", r);
+    let mut protocol = p.lock().unwrap();
+    println!("{}", res);
+    protocol.push(res);
+    drop(protocol);
+
     match result.f4 {
         Some(a) => match result.f6 {
             Some(c) => {
-                thread::spawn(move || h_task(tx, a, r, c, result1, "F"));
+                let mut protocol = p.lock().unwrap();
+                let initiates = String::from("F initiates H");
+                println!("{}", initiates);
+                protocol.push(initiates);
+                drop(protocol);
+                let ph = Arc::clone(&p);
+                thread::spawn(move || h_task(tx, ph, a, r, c, result1, "F"));
             }
             None => {}
         },
         None => {}
     }
 
-    println!("F ends at {}", Utc::now().time());
+    let mut protocol = p.lock().unwrap();
+    let end = format!("F ends at {}", Utc::now().time());
+    println!("{}", end);
+    protocol.push(end);
 }
 
 fn g_task(
     tx: mpsc::Sender<i32>,
+    p: Arc<Mutex<Vec<String>>>,
     n: i32,
     result2: Arc<Mutex<Result2>>,
     result1: Arc<Mutex<Result1>>,
 ) {
-    println!("G started at {}", Utc::now().time());
-    println!("G activated by Taks D");
-
-    thread::sleep(Duration::from_millis(DELAY));
+    let mut protocol = p.lock().unwrap();
+    let start = format!("G starts at {}", Utc::now().time());
+    println!("{}", start);
+    println!("G initiated by D");
+    protocol.push(start);
+    protocol.push(String::from("G initiated by D"));
+    drop(protocol);
 
     let mut result = result2.lock().unwrap();
     let r = n * 6;
     result.f6 = Some(r);
 
+    thread::sleep(Duration::from_millis(DELAY));
+
+    let res = format!("G result: {}", r);
+    let mut protocol = p.lock().unwrap();
+    println!("{}", res);
+    protocol.push(res);
+    drop(protocol);
+
     match result.f4 {
         Some(a) => match result.f5 {
             Some(b) => {
-                thread::spawn(move || h_task(tx, a, b, r, result1, "G"));
+                let mut protocol = p.lock().unwrap();
+                let initiates = String::from("G initiates H");
+                println!("{}", initiates);
+                protocol.push(initiates);
+                drop(protocol);
+                let ph = Arc::clone(&p);
+                thread::spawn(move || h_task(tx, ph, a, b, r, result1, "G"));
             }
             None => {}
         },
         None => {}
     }
 
-    println!("G ends at {}", Utc::now().time());
+    let mut protocol = p.lock().unwrap();
+    let end = format!("G ends at {}", Utc::now().time());
+    println!("{}", end);
+    protocol.push(end);
 }
 
 fn h_task(
     tx: mpsc::Sender<i32>,
+    p: Arc<Mutex<Vec<String>>>,
     n1: i32,
     n2: i32,
     n3: i32,
     result1: Arc<Mutex<Result1>>,
     parent: &str,
 ) {
-    println!("H started at {}", Utc::now().time());
-    println!("H activated by Taks {}", parent);
-
-    thread::sleep(Duration::from_millis(DELAY));
+    let mut protocol = p.lock().unwrap();
+    let start = format!("H starts at {}", Utc::now().time());
+    println!("{}", start);
+    println!("H initiated by {}", parent);
+    protocol.push(start);
+    protocol.push(format!("H initiated by {}", parent));
+    drop(protocol);
 
     let mut result = result1.lock().unwrap();
     let r = n1 + n2 + n3;
     result.f7 = Some(r);
 
+    thread::sleep(Duration::from_millis(DELAY));
+
+    let res = format!("H result: {}", r);
+    let mut protocol = p.lock().unwrap();
+    println!("{}", res);
+    protocol.push(res);
+    drop(protocol);
+
     match result.f1 {
         Some(a) => match result.f2 {
             Some(b) => {
-                thread::spawn(move || k_task(tx, a, b, r, "H"));
+                let mut protocol = p.lock().unwrap();
+                let initiates = String::from("H initiates K");
+                println!("{}", initiates);
+                protocol.push(initiates);
+                drop(protocol);
+                let pk = Arc::clone(&p);
+                thread::spawn(move || k_task(tx, pk, a, b, r, "H"));
             }
             None => {}
         },
         None => {}
     }
 
-    println!("H ends at {}", Utc::now().time());
+    let mut protocol = p.lock().unwrap();
+    let end = format!("H ends at {}", Utc::now().time());
+    println!("{}", end);
+    protocol.push(end);
 }
 
-fn k_task(tx: mpsc::Sender<i32>, n1: i32, n2: i32, n3: i32, parent: &str) {
-    println!("K started at {}", Utc::now().time());
-    println!("K activated by Taks {}", parent);
-
-    thread::sleep(Duration::from_millis(DELAY));
+fn k_task(
+    tx: mpsc::Sender<i32>,
+    p: Arc<Mutex<Vec<String>>>,
+    n1: i32,
+    n2: i32,
+    n3: i32,
+    parent: &str,
+) {
+    let mut protocol = p.lock().unwrap();
+    let start = format!("K starts at {}", Utc::now().time());
+    println!("{}", start);
+    println!("K initiated by {}", parent);
+    protocol.push(start);
+    protocol.push(format!("K initiated by {}", parent));
+    drop(protocol);
 
     let n = n1 + n2 + n3;
 
-    println!("Task K ends at {}", Utc::now().time());
+    thread::sleep(Duration::from_millis(DELAY));
+
+    let result = format!("H result: {}", n);
+    let mut protocol = p.lock().unwrap();
+    println!("{}", result);
+    protocol.push(result);
+    drop(protocol);
+
+    let mut protocol = p.lock().unwrap();
+    let end = format!("K ends at {}", Utc::now().time());
+    println!("{}", end);
+    protocol.push(end);
+
     tx.send(n).unwrap();
 }
